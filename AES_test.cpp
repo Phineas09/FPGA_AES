c33f922731d2d8d7752d788a70585b561a279912
Phineas09 <claudiu.ghenea15@yahoo.ro>
2023-01-29 22:41:48 +0200

    Finished with debug support
    Signed-off-by: Phineas09 <claudiu.ghenea15@yahoo.ro>

--- AES_test.cpp
+++ AES_test.cpp
@@ -5,7 +5,11 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <chrono>
+#include <ctime>
 #include "aes.h"
+
+#define DEBUG 0
 
 static void printHex(uint8_t *str)
 {
@@ -23,8 +27,33 @@ static int checkEquals(uchar *target, uchar *destination)
     return 0;
 }
 
+#ifdef DEBUG
+static void printToFile(std::fstream &outFile, uchar *value)
+{
+    std::stringstream hexValue;
+    for (int i = 0; i < 16; ++i)
+    {
+        hexValue << std::hex << std::setfill('0') << std::setw(2) << (int)value[i];
+    }
+    outFile << hexValue.str() << std::endl;
+}
+#endif
+
 static int executeEncryptionDecryptionTest(std::string fileName, int mode)
 {
+
+#ifdef DEBUG
+    std::fstream outFile;
+    outFile.open("D:\\Master\\Sem_3\\FPGA\\Proiect\\AES\\Sources\\debugFile.txt", std::ofstream::out | std::ios_base::app);
+    auto now = std::chrono::system_clock::now();
+    std::time_t timeNow = std::chrono::system_clock::to_time_t(now);
+    std::tm tm = *std::localtime(&timeNow);
+    // Timestamp like 2023-01-29 22:51:25
+    outFile << "Timestamp:\t"
+            << std::put_time(&tm, "%Y-%m-%d %H:%M:%S") << std::endl;
+
+    int round = 0;
+#endif
 
     int returnValue = 0;
     uchar key[16], goldenInput[16], goldenOutput[16], output[16];
@@ -43,7 +72,6 @@ static int executeEncryptionDecryptionTest(std::string fileName, int mode)
             std::string byte = line.substr(i * 2, 2);
             key[i] = (uchar)strtol(byte.c_str(), nullptr, 16);
         }
-
         while (std::getline(file, line))
         {
             ss.clear();
@@ -65,7 +93,7 @@ static int executeEncryptionDecryptionTest(std::string fileName, int mode)
                 goldenOutput[i] = (uchar)strtol(byte.c_str(), nullptr, 16);
             }
 
-            if (!mode)
+            if (mode == 0)
             {
                 // Encryption
                 // Key is the encryption key, goldenInput is the desired output
@@ -74,6 +102,17 @@ static int executeEncryptionDecryptionTest(std::string fileName, int mode)
                 AES_ECB(key, goldenInput, output, mode);
                 // Here, because this is encryption we compare the output to the desired goldenOutput.
                 returnValue += checkEquals(output, goldenOutput);
+
+#ifdef DEBUG
+                round++;
+                outFile << round << std::endl;
+                outFile << "Enc_Input:\t";
+                printToFile(outFile, goldenInput);
+                outFile << "Enc_OutG:\t";
+                printToFile(outFile, goldenOutput);
+                outFile << "Enc_Out:\t";
+                printToFile(outFile, output);
+#endif
             }
             else
             {
@@ -81,8 +120,24 @@ static int executeEncryptionDecryptionTest(std::string fileName, int mode)
                 AES_ECB(key, goldenOutput, output, mode);
                 // Here, because this is decryption we compare the output to the input that produces that encryption.
                 returnValue += checkEquals(goldenInput, output);
+#ifdef DEBUG
+                round++;
+                outFile << round << std::endl;
+                outFile << "Dec_Input:\t";
+                printToFile(outFile, goldenOutput);
+                outFile << "Dec_OutG:\t";
+                printToFile(outFile, goldenInput);
+                outFile << "Dec_Out:\t";
+                printToFile(outFile, output);
+#endif
             }
         }
+#ifdef DEBUG
+        outFile << "" << std::endl;
+        outFile << "" << std::endl;
+        outFile.close();
+#endif
+
         file.close();
     }
     else
@@ -95,6 +150,7 @@ int main()
 {
     // Each test will return 0 or 1, if all are 0 then all tests are correct, thare are 7 pairs of plain an cipher
     // Which makes a total of 14 tests (7 for enc and 7 for dec)
-    return executeEncryptionDecryptionTest("D:\\Master\\Sem_3\\FPGA\\Proiect\\AES\\Sources\\goldenValues.txt", 0) +
-           executeEncryptionDecryptionTest("D:\\Master\\Sem_3\\FPGA\\Proiect\\AES\\Sources\\goldenValues.txt", 1);
+    int returnValue = executeEncryptionDecryptionTest("D:\\Master\\Sem_3\\FPGA\\Proiect\\AES\\Sources\\goldenValues.txt", 0);
+    returnValue += executeEncryptionDecryptionTest("D:\\Master\\Sem_3\\FPGA\\Proiect\\AES\\Sources\\goldenValues.txt", 1);
+    return returnValue;
 }
